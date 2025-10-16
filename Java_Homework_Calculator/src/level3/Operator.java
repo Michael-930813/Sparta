bc85754e33aec4c6a6ac14154803903b242abbab
Michael-930813 <michael.930813@gmail.com>
2025-10-16 18:03:16 +0900

    Level3.Step1 : Create level3 Package, Modified Enumeration Structure

---
+++ Java_Homework_Calculator/src/level3/Operator.java
@@ -0,0 +1,31 @@
+package level3;
+
+public enum Operator {
+    // - Enumeration
+        PLUS('+'),
+        MINUS('-'),
+        MULTYPLE('*'),
+        DIVIDE('/');
+
+// - Variable
+    private final Character symbol;
+
+
+// - Function
+    // - Structor
+    Operator( char symbol ) {
+        this.symbol = symbol;
+    }
+    // - get & set
+    public Character getSymbol() { return symbol; }
+
+    // - Char -> Enum
+    public static Operator fromChar(char c) {
+        for(Operator op : Operator.values()) {
+            if(op.getSymbol() == c) {
+                return op;
+            }
+        }
+        return null;
+    }
+}
