6bd195eea8d28f86ca4c9d1b7f01ca55200c6e99
Michael-930813 <michael.930813@gmail.com>
2025-10-16 14:38:02 +0900

    Practice Level2.Step3 : AddFunction - Get & Set. Modified ClassStructure - Set result variable is List(Step1)

--- Java_Homework_Calculator/src/level2/Calculator.java
+++ Java_Homework_Calculator/src/level2/Calculator.java
@@ -1,39 +1,61 @@
 package level2;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Calculator {
 // - Variable
-    private Long result;
+    private Integer num0, num1;
+    private Character operator;
+    private List<Long> resultlist;
 
 
 // - Function
     // - Structor : Initialize
     public Calculator() {
-        result = 0L;
+        num0 = 0;
+        num1 = 0;
+        operator = null;
+        resultlist = new ArrayList<>();
+    }
+
+    // - Get & Set
+    public long getLastResult() {
+        return resultlist.get(resultlist.size()-1);
+    }
+    public void setNum0(Integer num0) {
+        this.num0 = num0;
+    }
+    public void setNum1(Integer num1) {
+        this.num1 = num1;
+    }
+    public void setOperator(Character operator) {
+        this.operator = operator;
     }
 
     // - Calculate
-    public long Calculate(long num0, long num1, char operator) {
-        switch(operator) {
+    public boolean Calculate() {
+        switch(this.operator) {
             case '+':
-                this.result = num0 + num1;
+                resultlist.add((long)(num0 + num1));
                 break;
             case '-':
-                this.result = num0 - num1;
+                resultlist.add((long)(num0 - num1));
                 break;
             case '*':
-                this.result = num0 * num1;
+                resultlist.add(num0.longValue() * num1.longValue());
                 break;
             case '/':
                 if( num1 == 0 ) {
                     System.out.println("0으로 나눌 수 없습니다.");
-                    return -1;
+                    return false;
                 }
-                this.result = num0 / num1;
+                resultlist.add((long)(num0 / num1));
                 break;
             default:
-                return -1;
+                return false;
         }
 
-        return result;
+        return true;
     }
 }
