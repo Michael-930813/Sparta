2ab0ef4955aa8792b3b58fe958c0a9728a6dc5ca
Michael-930813 <michael.930813@gmail.com>
2025-11-03 13:09:56 +0900

    Add SpringWeb_JPA Project

---
+++ SpringWeb_JPA/src/main/java/org/example/springweb_jpa/SpringWebJpaApplication.java
@@ -0,0 +1,13 @@
+package org.example.springweb_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebJpaApplication.class, args);
+    }
+
+}
