1d0a71a15252f17bd878c7638a532db2acf2754a
Michael-930813 <michael.930813@gmail.com>
2025-11-18 21:38:30 +0900

    CreateProject - SpringWeb_Scheduler (Homework Project)

---
+++ SpringWeb_Scheduler/src/main/java/sparta/scheduler/SpringWebSchedulerApplication.java
@@ -0,0 +1,13 @@
+package sparta.scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebSchedulerApplication.class, args);
+    }
+
+}
